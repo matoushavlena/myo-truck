2ee9046ba661f5852cd908600ae7c5fa917c6dd6
matoushavlena <havlenma@gmail.com>
2014-11-23 15:27:35 +0100

    changes

--- src/main/java/net/havlena/myotruck/MyoDataCollector.java
+++ src/main/java/net/havlena/myotruck/MyoDataCollector.java
@@ -1,7 +1,5 @@
 package net.havlena.myotruck;
 
-import java.rmi.RemoteException;
-
 import com.thalmic.myo.DeviceListener;
 import com.thalmic.myo.FirmwareVersion;
 import com.thalmic.myo.Myo;
@@ -14,54 +12,61 @@ import com.thalmic.myo.enums.VibrationType;
 import com.thalmic.myo.enums.XDirection;
 
 public class MyoDataCollector implements DeviceListener {
-    private static final int SCALE = 20;
+    public static final int SCALE = 20;
     private double rollW;
     private double pitchW;
     private double yawW;
     private Pose currentPose;
     private Arm whichArm;
+    private MyoTruck myotruck;
+    private boolean connected = false;
 
-    public MyoDataCollector() {
-	rollW = 0;
-	pitchW = 0;
-	yawW = 0;
-	currentPose = new Pose();
+    public MyoDataCollector(MyoTruck myotruck) {
+		rollW = 0;
+		pitchW = 0;
+		yawW = 0;
+		currentPose = new Pose();
+		this.myotruck = myotruck;
     }
 
     @Override
     public void onOrientationData(Myo myo, long timestamp, Quaternion rotation) {
-	Quaternion normalized = rotation.normalized();
+    	Quaternion normalized = rotation.normalized();
 
-	double roll = Math.atan2(2.0f * (normalized.getW() * normalized.getX() + normalized.getY() * normalized.getZ()), 1.0f - 2.0f * (normalized.getX() * normalized.getX() + normalized.getY() * normalized.getY()));
-	double pitch = Math.asin(2.0f * (normalized.getW() * normalized.getY() - normalized.getZ() * normalized.getX()));
-	double yaw = Math.atan2(2.0f * (normalized.getW() * normalized.getZ() + normalized.getX() * normalized.getY()), 1.0f - 2.0f * (normalized.getY() * normalized.getY() + normalized.getZ() * normalized.getZ()));
+		double roll = Math.atan2(2.0f * (normalized.getW() * normalized.getX() + normalized.getY() * normalized.getZ()), 1.0f - 2.0f * (normalized.getX() * normalized.getX() + normalized.getY() * normalized.getY()));
+		double pitch = Math.asin(2.0f * (normalized.getW() * normalized.getY() - normalized.getZ() * normalized.getX()));
+		double yaw = Math.atan2(2.0f * (normalized.getW() * normalized.getZ() + normalized.getX() * normalized.getY()), 1.0f - 2.0f * (normalized.getY() * normalized.getY() + normalized.getZ() * normalized.getZ()));
 
-	rollW = ((roll + Math.PI) / (Math.PI * 2.0) * SCALE);
-	pitchW = ((pitch + Math.PI / 2.0) / Math.PI * SCALE);
-	yawW = ((yaw + Math.PI) / (Math.PI * 2.0) * SCALE);
+		rollW = ((roll + Math.PI) / (Math.PI * 2.0) * SCALE);
+		pitchW = ((pitch + Math.PI / 2.0) / Math.PI * SCALE);
+		yawW = ((yaw + Math.PI) / (Math.PI * 2.0) * SCALE);
     }
 
     @Override
     public void onPose(Myo myo, long timestamp, Pose pose) {
-	currentPose = pose;
-	if (currentPose.getType() == PoseType.FIST) {
-	    myo.vibrate(VibrationType.VIBRATION_MEDIUM);
-	    MyoTruck.running = false;
-	}
-	if (currentPose.getType() == PoseType.FINGERS_SPREAD) {
-	    myo.vibrate(VibrationType.VIBRATION_SHORT);
-	    MyoTruck.running = true;
-	}
+		currentPose = pose;
+		if (currentPose.getType() == PoseType.FIST) {
+		    myo.vibrate(VibrationType.VIBRATION_MEDIUM);
+		    myotruck.sleep();
+		}
+		if (currentPose.getType() == PoseType.FINGERS_SPREAD) {
+		    myo.vibrate(VibrationType.VIBRATION_SHORT);
+		    myotruck.start();
+		}
+		if (currentPose.getType() == PoseType.THUMB_TO_PINKY) {
+		    myo.vibrate(VibrationType.VIBRATION_SHORT);
+		    myotruck.stop();
+		}
     }
 
     @Override
     public void onArmSync(Myo myo, long timestamp, Arm arm, XDirection xDirection) {
-	whichArm = arm;
+    	whichArm = arm;
     }
 
     @Override
     public void onArmUnsync(Myo myo, long timestamp) {
-	whichArm = null;
+    	whichArm = null;
     }
 
     @Override
@@ -70,11 +75,13 @@ public class MyoDataCollector implements DeviceListener {
 
     @Override
     public void onConnect(Myo myo, long timestamp, FirmwareVersion firmwareVersion) {
+    	connected = true;
     }
 
     @Override
     public void onDisconnect(Myo myo, long timestamp) {
-    	MyoTruck.running = true;
+    	connected = false;
+    	myotruck.stop();
     }
 
     @Override
@@ -83,7 +90,7 @@ public class MyoDataCollector implements DeviceListener {
 
     @Override
     public void onUnpair(Myo myo, long timestamp) {
-    	MyoTruck.running = true;
+    	myotruck.stop();
     }
 
     @Override
@@ -96,54 +103,52 @@ public class MyoDataCollector implements DeviceListener {
 
     @Override
     public String toString() {
-	StringBuilder builder = new StringBuilder("\r");
-
-	String xDisplay = String.format("[%s%s]", repeatCharacter('*', (int) rollW), repeatCharacter(' ', (int) (SCALE - rollW)));
-	String yDisplay = String.format("%1$,.2f", pitchW).concat(String.format("[%s%s]", repeatCharacter('*', (int) pitchW), repeatCharacter(' ', (int) (SCALE - pitchW))));
-	String zDisplay = String.format("[%s%s]", repeatCharacter('*', (int) yawW), repeatCharacter(' ', (int) (SCALE - yawW)));
-
-	String armString = null;
-	if (whichArm != null) {
-	    armString = String.format("[%s]", whichArm == Arm.ARM_LEFT ? "L" : "R");
-	} else {
-	    armString = String.format("[?]");
-	}
-	String poseString = null;
-	if (currentPose != null) {
-	    String poseTypeString = currentPose.getType()
-		    .toString();
-	    poseString = String.format("[%s%" + (SCALE - poseTypeString.length()) + "s]", poseTypeString, " ");
-	} else {
-	    poseString = String.format("[%14s]", " ");
-	}
-	builder.append(xDisplay);
-	builder.append(yDisplay);
-	builder.append(zDisplay);
-	builder.append(armString);
-	builder.append(poseString);
-	return builder.toString();
+		StringBuilder builder = new StringBuilder("\r");
+	
+		String xDisplay = "STEERING: ".concat(String.format("[%s%s]", repeatCharacter('*', (int) rollW), repeatCharacter(' ', (int) (SCALE - rollW))));
+		String yDisplay = " SPEED: ".concat(String.format("[%s%s]", repeatCharacter('*', (int) pitchW), repeatCharacter(' ', (int) (SCALE - pitchW))));
+	
+		String armString = null;
+		if (whichArm != null) {
+		    armString = " ARM: ".concat(String.format("[%s]", whichArm == Arm.ARM_LEFT ? "L" : "R"));
+		} else {
+		    armString = " ARM: ".concat(String.format("[?]"));
+		}
+		
+		String poseString = null;
+		if (currentPose != null) {
+		    String poseTypeString = currentPose.getType().toString();
+		    poseString = " POSE: ".concat(String.format("[%s%" + (SCALE - poseTypeString.length()) + "s]", poseTypeString, " "));
+		} else {
+		    poseString = " POSE: ".concat(String.format("[%14s]", " "));
+		}
+		builder.append(xDisplay);
+		builder.append(yDisplay);
+		builder.append(armString);
+		builder.append(poseString);
+		return builder.toString();
     }
 
     private String repeatCharacter(char character, int numOfTimes) {
-	StringBuilder builder = new StringBuilder();
-	for (int i = 0; i < numOfTimes; i++) {
-	    builder.append(character);
-	}
-	return builder.toString();
+		StringBuilder builder = new StringBuilder();
+		for (int i = 0; i < numOfTimes; i++) {
+		    builder.append(character);
+		}
+		return builder.toString();
     }
     
-    public void setSpeed() {
-	    try {
-	    	int speed = -400+(int)pitchW*50;
-			MyoTruck.motorL.setSpeed(speed);
-			if (speed>0) MyoTruck.motorL.backward();
-			else MyoTruck.motorL.forward();
-			MyoTruck.motorR.setSpeed(speed);
-			if (speed>0) MyoTruck.motorR.backward();
-			else MyoTruck.motorR.forward();
-		} catch (RemoteException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    public double getPitch() {
+    	if (!connected) return -1;
+    	return pitchW;
+	}
+    
+    public double getRoll() {
+    	if (!connected) return -1;
+    	return rollW;
+	}
+    
+    public boolean isLeftArm() {
+    	if (whichArm != null && whichArm == Arm.ARM_RIGHT) return false;
+    	return true;
 	}
 }
--- src/main/java/net/havlena/myotruck/MyoTruck.java
+++ src/main/java/net/havlena/myotruck/MyoTruck.java
@@ -4,100 +4,197 @@ import java.rmi.RemoteException;
 
 import com.thalmic.myo.Hub;
 import com.thalmic.myo.Myo;
-import com.thalmic.myo.example.DataCollector;
 
 import lejos.hardware.Button;
 import lejos.hardware.Sound;
+import lejos.hardware.lcd.GraphicsLCD;
+import lejos.hardware.lcd.LCD;
 import lejos.remote.ev3.RMIRegulatedMotor;
 import lejos.remote.ev3.RemoteEV3;
+import lejos.utility.Delay;
 
 public class MyoTruck {
-	
-	public static RMIRegulatedMotor motorW;
-	public static RMIRegulatedMotor motorL;
-	public static RMIRegulatedMotor motorR;
+	private RMIRegulatedMotor motorS;
+	private RMIRegulatedMotor motorL;
+	private RMIRegulatedMotor motorR;
 	private RemoteEV3 ev3;
-	public static boolean running = true;
-	private int totalSteps = 0;
+	private GraphicsLCD lcd;
+	private boolean running = true;
+	private boolean sleeping = false;
+	//private int totalSteps = 0;
+	// In case your maximum steering angles are shifted to either left or right, you can correct it here.
+	private static int ANGLE_STEERING_CORRECTION = 0;
+	// We don't want to allow steering to maximum values
+	private static int STEERING_RESERVE_FROM_MAX = 20;
+	// The higher number, the more sensitive, 1 is the lowest
+	private static int STEERING_SENSITIVITY = 3;
+	// The higher number, the more sensitive, 1 is the lowest
+	private static int SPEED_SENSITIVITY = 3;
+	private int steeringScale;  
 	
 	public MyoTruck(String host) throws RemoteException {
 		try {
 			this.ev3 = new RemoteEV3(host);
 			ev3.setDefault();
 			Sound.beep();
-			motorW = ev3.createRegulatedMotor("A", 'M');
+			Button.LEDPattern(4);
+			lcd = ev3.getGraphicsLCD();
+			lcd.clear();
+			lcd.drawString("Myo Truck", LCD.SCREEN_WIDTH/2, LCD.SCREEN_HEIGHT/2, GraphicsLCD.BASELINE | GraphicsLCD.HCENTER);
+			lcd.drawString("Running...", LCD.SCREEN_WIDTH/2, LCD.SCREEN_HEIGHT/2+30, GraphicsLCD.BASELINE | GraphicsLCD.HCENTER);
+			motorS = ev3.createRegulatedMotor("A", 'M');
 			motorL = ev3.createRegulatedMotor("B", 'L');
 			motorR = ev3.createRegulatedMotor("C", 'L');
 			
-			motorW.setSpeed((int)motorW.getMaxSpeed());
-			// RESET FRONT WHEELS TO CENTER POSITION
-			motorW.rotateTo(500);
-			motorW.resetTachoCount();
-			motorW.rotateTo(-130);
+			motorS.setSpeed((int)(motorS.getMaxSpeed()*0.8));
+			motorS.setAcceleration(3000);
+			// Reset front wheels to center position
+			motorS.rotateTo(-500);
+			int angleLeft = motorS.getTachoCount();
+			motorS.rotateTo(500);
+			int angleRight = motorS.getTachoCount();
+			steeringScale = (Math.abs(angleLeft)+Math.abs(angleRight))/2;
+			motorS.rotateTo(angleLeft+steeringScale+ANGLE_STEERING_CORRECTION);
+			steeringScale -= STEERING_RESERVE_FROM_MAX;
+			motorS.resetTachoCount();
+			motorS.rotateTo(-steeringScale);
+			motorS.rotateTo(steeringScale);
+			motorS.rotateTo(0);
 
-			//motorL.setSpeed((int)motorL.getMaxSpeed());
-			//motorR.setSpeed((int)motorR.getMaxSpeed());
-			motorL.setSpeed(10);
-			motorR.setSpeed(10);
-			motorL.setAcceleration(5000);
-			motorR.setAcceleration(5000);
+			motorL.setAcceleration(3000);
+			motorR.setAcceleration(3000);
+			motorL.setSpeed(0);
+			motorR.setSpeed(0);
 			motorL.stop(true);
 			motorR.stop(true);
-			motorL.backward();
-			motorR.backward();
 		} catch (Exception e) {
-			if (motorW!=null) motorW.close();
-			if (motorL!=null) motorL.close();
-			if (motorR!=null) motorR.close();  
+			this.closePorts();
 			e.printStackTrace();
 		}
 	}
 	
-	public void run(Hub hub, MyoDataCollector dataCollector) throws RemoteException {
+	private void closePorts() {
+		Button.LEDPattern(5);
+		try {
+			if (motorS!=null) motorS.close();
+			if (motorL!=null) motorL.close();
+			if (motorR!=null) motorR.close();  
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		lcd.clear();
+		lcd.drawString("Myo Truck", LCD.SCREEN_WIDTH/2, LCD.SCREEN_HEIGHT/2, GraphicsLCD.BASELINE | GraphicsLCD.HCENTER);
+		lcd.drawString("Closing...", LCD.SCREEN_WIDTH/2, LCD.SCREEN_HEIGHT/2+30, GraphicsLCD.BASELINE | GraphicsLCD.HCENTER);
+		Delay.msDelay(3000);
+		lcd.clear();
+		lcd.refresh();
+		Button.LEDPattern(0);
+	}
+	
+	private void run(Hub hub, MyoDataCollector dataCollector) throws RemoteException {
     	while (running) {
+    		hub.run(1000/20);
+    		System.out.print(dataCollector);
+    		if (sleeping==true) {
+    			try {
+        			Thread.sleep(200);
+        			continue;
+        		} catch (InterruptedException e) {
+        			e.printStackTrace();
+        		}
+    		}
     		/*totalSteps++;
-    		if (totalSteps>200) {
+    		if (totalSteps>100) {
     			running = false;
     			break;
     		}*/
-	    	hub.run(1000/10);
 	    	System.out.print(dataCollector);
-	    	dataCollector.setSpeed();
-    		//try {
-    		//	Thread.sleep(50);
-    		//} catch (InterruptedException e) {
-    		//	e.printStackTrace();
-    		//}
+	    	this.setSpeed(dataCollector.getPitch());
+	    	this.setSteering(dataCollector.getRoll(), dataCollector.isLeftArm());
     	}
     	motorL.stop(true);
 		motorR.stop(true);
-		if (motorW!=null) motorW.close();
-		if (motorL!=null) motorL.close();
-		if (motorR!=null) motorR.close(); 
+	}
+	
+	private void setSpeed(double pitch) {
+		try {
+			int speed = 0;
+			int scalePitch = MyoDataCollector.SCALE/2/SPEED_SENSITIVITY;
+			int scaleMotor = (int)motorL.getMaxSpeed();
+			if (pitch>=0) speed = (int)((-scalePitch*SPEED_SENSITIVITY+pitch)*(scaleMotor/scalePitch));
+			motorL.setSpeed(speed);
+			motorR.setSpeed(speed);
+			if (speed>=0) {
+				motorL.backward();
+				motorR.backward();
+			} else {
+				motorL.forward();
+				motorR.forward();
+			}
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void setSteering(double roll, boolean isLeftArm) {
+		try {
+			int steering = 0;
+			int scale = MyoDataCollector.SCALE/2/STEERING_SENSITIVITY;
+			if (roll>=0) steering = (int)((-scale*STEERING_SENSITIVITY+roll)*(steeringScale/scale));
+			if (isLeftArm) steering = steering*-1;
+			motorS.rotateTo(steering);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void stop() {
+		this.running = false;
+	}
+	
+	public void sleep() {
+		lcd.clear();
+		lcd.drawString("Myo Truck", LCD.SCREEN_WIDTH/2, LCD.SCREEN_HEIGHT/2, GraphicsLCD.BASELINE | GraphicsLCD.HCENTER);
+		lcd.drawString("Sleeping...", LCD.SCREEN_WIDTH/2, LCD.SCREEN_HEIGHT/2+30, GraphicsLCD.BASELINE | GraphicsLCD.HCENTER);
+		Button.LEDPattern(6);
+		try {
+			motorL.setSpeed(0);
+			motorR.setSpeed(0);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}		
+		this.running = true;
+		this.sleeping = true;
+	}
+	
+	public void start() {
+		Button.LEDPattern(4);
+		this.running = true;
+		this.sleeping = false;
+	}
+	
+	protected void finalize() throws Throwable {
+		try {
+			this.closePorts();
+		} finally {
+			super.finalize();
+		}
 	}
 	
 	public static void main(String[] args) {
 		try {
 			MyoTruck myotruck = new MyoTruck("192.168.192.5");
-			
 			Hub hub = new Hub("net.havlena.myo");
-		    System.out.println("Attempting to find a Myo...");
 		    Myo myo = hub.waitForMyo(10000);
 		    if (myo == null) {
 				throw new RuntimeException("Unable to find a Myo!");
 			}
-		    
-		    System.out.println("Connected to a Myo armband!");
-		    MyoDataCollector dataCollector = new MyoDataCollector();
+		    MyoDataCollector dataCollector = new MyoDataCollector(myotruck);
 		    hub.addListener(dataCollector);
 		    myotruck.run(hub, dataCollector);
-		    //while (true) {
-
-		    //}
+		    myotruck.closePorts();
 		} catch (Exception e) {
 			e.printStackTrace();
-			System.exit(1);
 		}
 	}
-  
 }
